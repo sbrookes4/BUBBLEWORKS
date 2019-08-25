f45272feacfc0c3ab7fb1b3946b98a5903ac9ace
Shawna Brookes <shawnabrookes@gmail.com>
2019-08-24 22:24:39 -0700

    init commit'

---
+++ BUBBLEWORKS.js
@@ -0,0 +1,317 @@
+
+
+$(document).ready(function(){
+
+$("button").click(function(){
+
+	$("#BtuutonX").fadeOut();
+/*------------------------------------------------------------------------*/
+
+$("#b1").animate({left: '0px', bottom: '120vh'}, 8000);
+
+
+setInterval(function(){ 
+$("#b1").hide();
+}, 8000);
+/*------------------------------------------------------------------------*/
+
+$("#b2").animate({left: '0px', bottom: '120vh'}, 7000);
+
+setInterval(function(){ 
+$("#b2").hide();
+}, 7000);
+
+
+/*------------------------------------------------------------------------*/
+
+$("#b3").animate({left: '0px', bottom: '120vh'}, 10000);
+
+setInterval(function(){ 
+$("#b3").hide();
+}, 11000);
+
+
+/*------------------------------------------------------------------------*/
+$("#b4").animate({left: '0px', bottom: '120vh'}, 9000);
+
+setInterval(function(){ 
+$("#b4").hide();
+}, 9000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b5").animate({left: '0px', bottom: '120vh'}, 11000);
+
+setInterval(function(){ 
+$("#b5").hide();
+}, 10000);
+
+/*------------------------------------------------------------------------*/
+$("#b6").animate({left: '0px', bottom: '120vh'}, 5000);
+
+setInterval(function(){ 
+$("#b6").hide();
+}, 5000);
+
+
+/*------------------------------------------------------------------------*/
+$("#b7").animate({left: '0px', bottom: '120vh'}, 9000);
+
+setInterval(function(){ 
+$("#b7").hide();
+}, 7500);
+
+/*------------------------------------------------------------------------*/
+$("#b8").animate({left: '0px', bottom: '120vh'}, 9500);
+
+setInterval(function(){ 
+$("#b8").hide();
+}, 9500);
+/*------------------------------------------------------------------------*/
+$("#b9").animate({left: '0px', bottom: '120vh'}, 11000);
+
+setInterval(function(){ 
+$("#b9").hide();
+}, 8000);
+
+/*------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------*/
+
+$("#b10").animate({left: '0px', bottom: '0px'}, 7000);
+$("#b10").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b10").hide();
+}, 8000);
+
+
+/*------------------------------------------------------------------------*/
+$("#b11").animate({left: '0px', bottom: '0px'}, 7500);
+$("#b11").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b11").hide();
+}, 8500);
+/*------------------------------------------------------------------------*/
+
+$("#b12").animate({left: '0px', bottom: '0px'}, 8000);
+$("#b12").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b12").hide();
+}, 9000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b13").animate({left: '0px', bottom: '0px'}, 8500);
+$("#b13").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b13").hide();
+}, 9500);
+
+/*------------------------------------------------------------------------*/
+
+$("#b14").animate({left: '0px', bottom: '0px'}, 9000);
+$("#b14").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b14").hide();
+}, 10000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b15").animate({left: '0px', bottom: '0px'}, 9500);
+$("#b15").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b15").hide();
+}, 10500);
+
+/*------------------------------------------------------------------------*/
+
+$("#b16").animate({left: '0px', bottom: '0px'}, 10000);
+$("#b16").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b16").hide();
+}, 11000);
+/*------------------------------------------------------------------------*/
+
+$("#b17").animate({left: '0px', bottom: '0px'}, 10500);
+$("#b17").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b17").hide();
+}, 11500);
+/*------------------------------------------------------------------------*/
+
+$("#b18").animate({left: '0px', bottom: '0px'}, 11000);
+$("#b18").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b18").hide();
+}, 12000);
+/*------------------------------------------------------------------------*/
+
+$("#b19").animate({left: '0px', bottom: '0px'}, 11500);
+$("#b19").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b19").hide();
+}, 12500);
+/*------------------------------------------------------------------------*/
+
+$("#b20").animate({left: '0px', bottom: '0px'}, 12000);
+$("#b20").animate({left: '0px', bottom: '1050px'}, 1200);
+setInterval(function(){ 
+$("#b20").hide();
+}, 13000);
+/*------------------------------------------------------------------------*/
+/*------------------------------------------------------------------_______________________________________________------*/
+
+$("#b21").animate({left: '0px', bottom: '0px'}, 7000);
+$("#b21").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b21").animate({left: '0px', bottom: '75vh'}, 1000); 
+$("#b21").animate({left: '0px', bottom: '76vh'}, 1000);  
+$("#b21").animate({left: '0px', bottom: '73vh'}, 700);   
+$("#b21").animate({left: '0px', bottom: '72vh'}, 1000); 
+$("#b21").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b21").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b21").hide();
+}, 23000);
+
+
+/*------------------------------------------------------------------------*/
+$("#b22").animate({left: '0px', bottom: '0px'}, 7500);
+$("#b22").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b22").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b22").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b22").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b22").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b22").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b22").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b22").hide();
+}, 23000);
+/*------------------------------------------------------------------------*/
+
+$("#b23").animate({left: '0px', bottom: '0px'}, 8000);
+$("#b23").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b23").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b23").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b23").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b23").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b23").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b23").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b23").hide();
+}, 23000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b24").animate({left: '0px', bottom: '0px'}, 8500);
+$("#b24").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b24").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b24").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b24").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b24").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b24").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b24").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b24").hide();
+}, 23000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b25").animate({left: '0px', bottom: '0px'}, 9000);
+$("#b25").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b25").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b25").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b25").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b25").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b25").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b25").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b25").hide();
+}, 23000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b26").animate({left: '0px', bottom: '0px'}, 9500);
+$("#b26").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b26").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b26").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b26").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b26").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b26").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b26").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b26").hide();
+}, 23000);
+
+/*------------------------------------------------------------------------*/
+
+$("#b27").animate({left: '0px', bottom: '0px'}, 10000);
+$("#b27").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b27").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b27").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b27").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b27").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b27").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b27").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b27").hide();
+}, 23000);
+/*------------------------------------------------------------------------*/
+
+$("#b28").animate({left: '0px', bottom: '0px'}, 10500);
+$("#b28").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b28").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b28").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b28").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b28").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b28").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b28").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b28").hide();
+}, 23000);
+/*------------------------------------------------------------------------*/
+
+$("#b29").animate({left: '0px', bottom: '0px'}, 11000);
+$("#b29").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b29").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b29").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b29").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b29").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b29").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b29").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b29").hide();
+}, 23000);
+/*------------------------------------------------------------------------*/
+
+$("#b30").animate({left: '0px', bottom: '0px'}, 11500);
+$("#b30").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b30").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b30").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b30").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b30").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b30").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b30").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b30").hide();
+}, 23000);
+/*------------------------------------------------------------------------*/
+
+$("#b31").animate({left: '0px', bottom: '0px'}, 12000);
+$("#b31").animate({left: '0px', bottom: '78vh'}, 4000);
+$("#b31").animate({left: '0px', bottom: '75vh'}, 1000);
+$("#b31").animate({left: '0px', bottom: '76vh'}, 1000);
+$("#b31").animate({left: '0px', bottom: '68vh'}, 700);
+$("#b31").animate({left: '0px', bottom: '72vh'}, 1000);
+$("#b31").animate({left: '0px', bottom: '71vh'}, 500); 
+$("#b31").animate({left: '0px', bottom: '70vh'}, 700);
+setInterval(function(){ 
+$("#b31").hide();
+}, 23000);
+
+
+});//button click
+
+
+});//overall
+
+
